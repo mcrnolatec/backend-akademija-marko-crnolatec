48462703667579b3e1c9f91fc53ffe8f66f63040
Marko Crnolatec <mcrnolate21@student.foi.hr>
2026-07-01 07:58:04 +0200

    Initialize project Java 25 and Spring Boot with Spring Web and Validation dependencies.

---
+++ src/test/java/hr/crnolatec/akademija/BackendAkademijaApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.crnolatec.akademija;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAkademijaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
